477baad0bf6ef1582ad2270205f6e35ce17bd855
Fiona Ye <fiona.ye@yandex.com>
2020-09-16 22:54:54 +0800

    make datdot-ui-tab component

--- bundle.js
+++ bundle.js
@@ -1,7 +1,13 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 localStorage.debug = '*' // to get debug output
 const datdotui = require('..')
-const element = datdotui({ buttons: 200 })
+const data = {
+    jobs: {
+      title: 'JOBS',
+      tab: ['Calendar', 'Summary', 'Performance']
+    }
+  }
+const element = datdotui(data)
 document.body.append(element)
 },{"..":30}],2:[function(require,module,exports){
 var trailingNewlineRegex = /\n[\s]+$/
@@ -1965,147 +1971,148 @@ process.umask = function() { return 0; };
 const debug = require('debug')
 const bel = require('bel')
 const csjs = require('csjs-inject')
-
-const datdot_ui_button = require('datdot-ui-button')
+const tab = require('datdot-ui-tab')
 
 module.exports = datdotui
 
 function datdotui (opts) {
 
-  const name = `datdot-ui-${`${Math.random()}`.slice(2)}>`
-  const log = debug(name)
-  var counter = 0
-  var lastmessage
-  const state = {}
+  const { jobs } = opts
+  let state = {}
 
+  const element = bel`
+  <div>
+    ${tab({page: jobs.title, arr: jobs.tab, protocol: protocolTab})}
+    <div class=${css.terminal}></div>
+  </div>
+  `
+  const [tabs, terminal] = element.children
+ 
+  function protocolTab(message) {
+    let { from, flow, type, body, active} = message
+    const log = debug(from)
+    const logger = log.extend('datdot-ui')
+    // logger.log must be put first then logger()
+    logger.log = domlog
+    logger(message)
+  
+    if ( state.tab == undefined ) state.tab = new Array()
 
-  for(var buttons = [], i = opts.buttons || 1; i; i--) {
-    const btn_name = `btn-${i}`
-    const btn_text = i
-    const protocol = send => {
-      const logger = log.extend(btn_name)
-      logger.log = domlog
-      state[btn_name] = { send, logger }
-      return hear
-    }
-    const button = datdot_ui_button({ name: btn_name, text: btn_text, protocol })
-    buttons.push(button)
+    const found = state.tab.some( el => el.body === body)
+    if (!found) state.tab.push( {id: `datdot-ui-${`${Math.random()}`.slice(2)}`, from, flow, type, body, active, logger} )
+
+    state.tab.map( el => { 
+      if ( el.body === body && el.active ) return
+      el.body !== body ? el.active = false : el.active = true 
+    })
+    
+    // check state 
+    const tabChanges = log.extend('ui-tab changes')
+    tabChanges(state.tab)
+    
+    return receive(message)
   }
 
+  function receive(message) {
+    const { from, flow, type, body, active} = message
+    const log = debug(from)
+    const logger = log.extend('receive>')
+    logger(flow, type, body, active)
+  }
 
-  const element = bel`<div class=${css.ui}>
-    <div class=${css.buttons}>${buttons}</div>
-    <div class=${css.terminal}></div>
-  </div>`
-  const [buttonfield, terminal] = element.children
-
+  function domlog(...args) {
+    const {from, flow, type, body} = args[3]
+    const el = bel`<p>${from + " > "}  ${flow}: ${body} ${type}</p>`
+    terminal.append(el)
+  }
 
   return element
-
-
-  function hear (message) {
-    const { flow: [from, id], type, body } = message
-    if (!state[from]) log('unknown sender', bel`<pre>${JSON.stringify(message, 0, 2)}</pre>`)
-    state[from].logger(id, type, body)
-    if (type === 'click') {
-      const text = body
-      if (!lastmessage) return lastmessage = { lastname: from, lasttext: text }
-      const { lastname, lasttext } = lastmessage
-      log('flip buttons:', from, lastname)
-      state[from].send({ flow: [name, counter++], type: 'update', body: { key: 'text', value: lasttext } })
-      state[lastname].send({ flow: [name, counter++], type: 'update', body: { key: 'text', value: text } })      
-      lastmessage = void 0
-    }
-  }
-
-
-  function domlog (...args) {
-    const [_0, _1, _2, id, _3, type, btn2] = args
-    console.log({_0, _1, _2, id, _3, type, btn2})
-    var element = bel`<p>${name} ${type} button ${btn2}</p>`
-    terminal.append(element)
-  }
-
 
 }
 const css = csjs`
 body { 
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   margin: 0;
   padding: 0;
-  background-color: black;
-  height: 100vh;
-  width: 100vw;
-}
-.ui {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 80vw;
-  background-color: red;
+  background-color: #F2F2F2;
 }
 .terminal {
-  background-color: grey;
-  min-height: 200px;
-  max-height: 200px;
-  overflow-y: scroll;
-  height: auto;
-  color: black;
-  width: 100%;
-}`
-
-},{"bel":3,"csjs-inject":6,"datdot-ui-button":31,"debug":23}],31:[function(require,module,exports){
-const debug = require('debug')
-const bel = require('bel')
-const csjs = require('csjs-inject')
-
-module.exports = datdot_ui_button
-
-function datdot_ui_button (opts) {
-  const { name = `<annonymous-${`${Math.random()}`.slice(2)}>`, protocol } = opts
-  const log = debug(name)
-  var counter = 0
-
-
-  var text = opts.text || name
-  const send = protocol(hear)
-  const element = bel`<button class=${css.btn}>${text}</button>`
-  element.onclick = onclick
-
-
-  return element
-
-
-  function hear (message) {
-    const { flow: [from, id], type, body } = message
-    log_from2self = log.extend(from)
-    log_from2self(id, type, body)
-    if (type === 'update') {
-      const { key, value } = body
-      if (key !== 'text') return
-      log('update text:', value)
-      element.textContent = text = value
-    }
-  }
-
-
-  function onclick (event) {
-    const message = { flow: [name, counter++], type: 'click', body: text }
-    log('send', message)
-    send(message)
-  }
-
 
 }
-const css = csjs`
-.btn {
-  color: green;
-  width: 50px;
-  height: 50px;
-}`
+`
 
+},{"bel":3,"csjs-inject":6,"datdot-ui-tab":31,"debug":23}],31:[function(require,module,exports){
+const bel = require(('bel'))
+const csjs = require('csjs-inject')
+const debug = require('debug')
+
+module.exports = datdot_ui_tab
+
+function datdot_ui_tab(opts) {
+    const { page, arr, protocol } = opts
+    const element = bel`
+    <nav class=${css.tab}>
+        ${arr.map( (name, index) => {
+                if (index === 0) {
+                    return bel`<button class="${css.btn} ${css.current}" onclick=${ (e) => onTrigger(e, name) }>${name}</button>`
+                } else {
+                    return bel`<button class=${css.btn} onclick=${ (e) => onTrigger(e, name) }>${name}</button>`
+                }
+            })
+        }
+    </nav>
+    `
+
+    return element
+
+    function onTrigger(e, name) {
+        const ui = 'ui-tab'
+        const log = debug(page)
+        const logger = log.extend(`${ui} > ${name}`)
+        let message = { from: page, flow: ui, type: 'click', body: name, active: false}
+        const el = e.target
+        const childrens = [...element.children]
+
+        if (el.classList.contains(css.current)) return
+            // add the code below if want to display log on console
+            // message.active = true
+
+        childrens.forEach( btn => {
+            btn.classList.remove(css.current)
+            message.active = false
+        })
+
+        if ( el.innerText === name ) {
+            el.classList.add(css.current)
+            message.active = true
+        }
+
+        logger('send', message)
+        return protocol(message)
+    }
+
+}
+
+const css = csjs`
+.tab {
+    
+}
+.btn {
+    color: ##000000;
+    background-color: #d9d9d9;
+    padding: 15px;
+    border: none;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    margin-right: 4px;
+    outline: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease-in-out;
+}
+.btn:hover {
+    background-color: #afafaf;
+}
+.current, .current:hover {
+    background-color: #fff;
+}
+`
 },{"bel":3,"csjs-inject":6,"debug":23}]},{},[1]);
--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,10 @@
 localStorage.debug = '*' // to get debug output
 const datdotui = require('..')
-const element = datdotui({ buttons: 200 })
+const data = {
+    jobs: {
+      title: 'JOBS',
+      tab: ['Calendar', 'Summary', 'Performance']
+    }
+  }
+const element = datdotui(data)
 document.body.append(element)
